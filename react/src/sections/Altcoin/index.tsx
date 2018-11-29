089a9834e9e6705bcadffe1d2c9e7cbeb5d7c4b4
Luke Pighetti <lukepighetti@gmail.com>
2018-11-28 19:40:49 -0500

    stats and viewport scale

--- react/src/sections/Altcoin/index.tsx
+++ react/src/sections/Altcoin/index.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
-// import * as icon from "./icon.svg";
 import LatestDataContainer from "./service";
-import { x16s } from "./utils";
+import { x16s, x16sCycle } from "./utils";
 
 export default class AltcoinSection extends React.Component {
   render() {
@@ -12,27 +11,37 @@ export default class AltcoinSection extends React.Component {
           <Title>
             <span>X16S</span>
             <br />
-            algorithm
+            shuffle mining
             <br />
-            shuffle
+            algorithm
           </Title>
-          <Subtitle>debuted in pigeoncoin</Subtitle>
-          <Table>
-            <LatestDataContainer>
-              {({ chain }) =>
-                x16s(chain.lastHash).map(({ hex, algorithm }) => (
-                  <li key={hex}>{algorithm}</li>
-                ))
-              }
-            </LatestDataContainer>
-          </Table>
+          <Subtitle>power & hashrate consistency</Subtitle>
+          <Aside>debuted on pigeoncoin</Aside>
 
-          {/* <Logo src={icon} /> */}
+          <LatestDataContainer>
+            {({ chain, market }) => (
+              <>
+                <Table>
+                  {x16s(chain.lastHash).map(({ hex, algorithm }) => (
+                    <li key={hex}>{algorithm}</li>
+                  ))}
+                </Table>
+                <Stats>
+                  <div>{(chain.hashrate / 20e6).toFixed(0)} miners</div>
+                  <div>{chain.height.toFixed(0)} blocks</div>
+                  <div>{market.marketCapUsd.toFixed(0)} dollars</div>
+                  <h3>{x16sCycle(chain.lastHash)}</h3>
+                </Stats>
+              </>
+            )}
+          </LatestDataContainer>
         </Grid>
       </Section>
     );
   }
 }
+
+const border = `0.4rem solid rgba(255, 255, 255, 0.8)`;
 
 /**
  * Styled Components
@@ -100,19 +109,47 @@ const Subtitle = styled.h2`
   writing-mode: vertical-rl;
 `;
 
+const Aside = styled.h2`
+  font-size: 1.4rem;
+  text-align: right;
+
+  grid-column: 2/6;
+  grid-row: 1;
+
+  border-top: ${border};
+  border-bottom: ${border};
+
+  padding: 0.14rem;
+  align-self: end;
+  transform: translateY(-10rem);
+
+  max-width: 40rem;
+`;
+
 const Table = styled.ul`
   grid-column: 1/2;
-  grid-row: 2/6;
+  grid-row: 3/6;
 
   list-style-type: none;
   line-height: 1.2;
-
-  transform: rotate(-45deg) translate(40px, 40px);
+  align-self: end;
 
   li {
     font-weight: 700;
     margin-top: 0.14rem;
 
-    border-bottom: 0.4rem solid rgba(255, 255, 255, 0.8);
+    border-bottom: ${border};
   }
+`;
+
+const Stats = styled.div`
+  text-align: right;
+  grid-column: 2/7;
+  grid-row: 4/6;
+
+  font-weight: 300;
+
+  align-self: end;
+  transform: translateY(0.2rem);
+  word-break: break-all;
 `;
